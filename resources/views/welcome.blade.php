3e14dbd039ee68b9efe1cfc109212e904acb515f
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2021-10-28 11:28:08 +0330

    fixing laravel name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LARAVEL
+                    Laravel
                 </div>
 
                 <div class="links">
